8b82f3ab661b92e0621de6004c8c064867309c46
TomLucasakaTGeek <TanmayEmpowers@proton.me>
2025-10-05 23:50:26 +0530

    n

--- Server/routes/userRoutes.js
+++ Server/routes/userRoutes.js
@@ -2,11 +2,13 @@ import express from "express";
 import { auth } from "../middlewares/auth";
 import { getUserCreations, getPublishedCreations, toggleLikeCreation } from "../controllers/userController";
 
+
 const userRouter = express.Router()
+
 
 userRouter.get('/get-user-creations', auth, getUserCreations)
 userRouter.get('/get-published-creations', auth, getPublishedCreations)
-userRouter.get('/toggle-like-creations', auth, toggleLikeCreation)
+userRouter.post('/toggle-like-creations', auth, toggleLikeCreation)
 
 
 export default userRouter; 
