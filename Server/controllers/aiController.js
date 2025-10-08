ba5875e05ac989f732c29959c9e977906027acd6
TomLucasakaTGeek <TanmayEmpowers@proton.me>
2025-10-08 22:34:34 +0530

    making more changes

--- Server/controllers/aiController.js
+++ Server/controllers/aiController.js
@@ -204,7 +204,7 @@ export const generateImage = async (req, res) => {
 export const removeImageBackground = async (req, res) => {
     try {
         const { userId } = req.auth()
-        const { image } = req.file
+        const image = req.file
         const plan = req.plan;
         
 
@@ -258,7 +258,7 @@ export const removeImageObject = async (req, res) => {
     try {
         const { userId } = req.auth()
         const { object } = req.body()
-        const { image } = req.file
+        const image = req.file
         const plan = req.plan;
         
 
